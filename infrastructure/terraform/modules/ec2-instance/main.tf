e5bc7a7cd789489ed57d567aa70568bcb1430d6d
Blacknash901 <juanantonio901@gmail.com>
2025-11-24 19:24:21 -0600

    manifest and SG fix

--- infrastructure/terraform/modules/ec2-instance/main.tf
+++ infrastructure/terraform/modules/ec2-instance/main.tf
@@ -35,6 +35,30 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Monitor App"
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     description = "Allow all outbound traffic"
     from_port   = 0
